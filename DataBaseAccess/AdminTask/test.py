8be3863c89bc5f9723982ffd8e09ff2529ce5591
ChoiJeongwon <jw8choi@uwaterloo.ca>
2016-03-03 00:47:27 -0500

    3.14 side two way communication

--- DataBaseAccess/AdminTask/test.py
+++ DataBaseAccess/AdminTask/test.py
@@ -1,12 +1,19 @@
 import serial
+import time
 
 
 def main():
-	ser = serial.Serial('/dev/tty.usbmodemfa131', 9600)
-	# ser.open()
+	ser = serial.Serial('/dev/tty.usbmodem1451', 9600, timeout=3)
 	
 	while True:
-		print(ser.readline())
+		time.sleep(3)
+		line = ser.readline()
+		print("messageFromArduino: " + str(line))
+		ser.write(b"ResponseFromPi")
+		
+		returnline = ser.readline()
+		print("returningMessage  :" + str(returnline))
+		print("ser.in_waiting    :" + str(ser.in_waiting) + "\n\n")
 
 	ser.close()
 
