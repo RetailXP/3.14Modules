ad1d20e6b44bcf63d0cc9e7484cc799eec003f46
ChoiJeongwon <jw8choi@uwaterloo.ca>
2016-03-16 01:50:48 -0400

    Communication implementation

--- DataBaseAccess/AdminTask/ArduinoComm.py
+++ DataBaseAccess/AdminTask/ArduinoComm.py
@@ -1,4 +1,5 @@
 import serial
+import time
 import queue
 from AdminTask.MessageParser import *
 from AdminTask.MessageFormat import *
@@ -17,8 +18,6 @@ class ArduinoComm(Thread):
 		self.msgQueue = queue.Queue(0)  # infinite queue size
 		self.msgInProcess = None		# msg received
 
-		self.isWaitingMsg = False		# msg from arduino
-
 		self.start()
 
 	def __del__(self):
@@ -35,101 +34,101 @@ class ArduinoComm(Thread):
 		while True:
 
 			## sending a message to Arduino
-			if not self.isWaitingMsg and not self.msgQueue.empty():
+			while self.msgQueue.empty():
+				time.sleep(1)	# poll the queue every 1 second
 				
-				# [ MessageFormat.retInv
-				# (True, 
-				#  [inventoryDetailsId, barcodeDetailsFk, X_index, Y_index, X_encoder, Y_encoder, checkoutFlag],
-				#  newVirtualCartRowId)
-				# ]
+			# [ MessageFormat.retInv
+			# (True, 
+			#  [inventoryDetailsId, barcodeDetailsFk, X_index, Y_index, X_encoder, Y_encoder, checkoutFlag],
+			#  newVirtualCartRowId)
+			# ]
 
-				# [ MessageFormat.depInv, barcode ]
+			# [ MessageFormat.depInv, barcode ]
 
-				msgQueue = self.msgQueue.get()
-				msgType = msgQueue[0]
-				msgInProgress = None
-				encodedMsg = None
+			msgQueue = self.msgQueue.get()
+			msgType = msgQueue[0]
+			txMsg = None
+			encodedMsg = None
+
+			if msgType == MessageFormat.retInv:
+				reservedInventoryLoc = msgQueue[1][1]
+				virtCartRowId = msgQueue[1][2]
+
+				txMsg = RetInvP2A()
+				txMsg.invInfoRowId = reservedInventoryLoc[0]
+				txMsg.virtCartId = virtCartRowId
+				txMsg.x_idx = reservedInventoryLoc[1]
+				txMsg.y_idx = reservedInventoryLoc[2]
+				txMsg.x_enc = reservedInventoryLoc[3]
+				txMsg.y_enc = reservedInventoryLoc[4]
+				txMsg.x_encAbove = reservedInventoryLoc[5]
+				txMsg.y_encAbove = reservedInventoryLoc[6]
+
+				self.msgInProcess = [MessageFormat.retInv, txMsg]
+
+			elif msgType == MessageFormat.depInv:
+				barcode = msgQueue[1]
+
+				robotService = ArduinoService()
+				[x_idx, y_idx, x_enc, y_enc] = robotService.getDepositLocation()
+
+				txMsg = DepoInvP2A()
+				txMsg.barcode = barcode
+				txMsg.x_enc = x_enc
+				txMsg.y_enc = y_enc
+
+				self.msgInProcess = [MessageFormat.depInv, (barcode, x_idx, y_idx, x_enc, y_enc)]
+
+			try:
+				encodedMsg = txMsg.encode()
+			except CorruptData as details:
+				print(details)
+
+
+			for byte in encodedMsg:
+				self.__ser.write(byte)
+
+
+			## receiving message from arduino
+			msgParser = MessageParser()
+
+			isCompleteMsg = False
+			while not isCompleteMsg:
+				msgByte = self.__ser.read(1)		# blocking i/o with infinite timeout
+				parseResult = msgParser.parseMsg(msgByte)
+				isCompleteMsg = parseResult[0]
+
+			parseResult = msgParser.getRetVal()		# [True, msgType, msgContent]
+
+			msgType = parseResult[1]
+			msgContent = parseResult[2]
+
+			try:
+				robotService = ArduinoService()
 
 				if msgType == MessageFormat.retInv:
-					reservedInventoryLoc = msgQueue[1][1]
-					virtCartRowId = msgQueue[1][2]
-
-					msgInProgress = RetInvP2A()
-					msgInProgress.invInfoRowId = reservedInventoryLoc[0]
-					msgInProgress.virtCartId = virtCartRowId
-					msgInProgress.invInfoRowId = reservedInventoryLoc[1]
-					msgInProgress.x_idx = reservedInventoryLoc[2]
-					msgInProgress.y_idx = reservedInventoryLoc[3]
-					msgInProgress.x_enc = reservedInventoryLoc[4]
-					msgInProgress.y_enc = reservedInventoryLoc[5]
-					msgInProgress.x_encAbove = reservedInventoryLoc[6]
-					msgInProgress.y_encAbove = reservedInventoryLoc[7]
-
-					self.msgInProcess = [MessageFormat.retInv, msgInProgress]
+					msg = RetInvA2P()
+					msg.decode(msgContent)
+					service.processRetrievedInventory(msg.invInfoRowId, msg.virtCartId)
 
 				elif msgType == MessageFormat.depInv:
-					barcode = msgQueue[1]
+					msg = DepInvA2P()
+					msg.decode(msgContent)
+					#(barcode, x_index, y_index, x_encoder, y_encoder):
+					txMsgContent = self.msgInProcess[1]
+					service.depositInventory(txMsgContent[0],
+											 txMsgContent[1],
+											 txMsgContent[2],
+											 txMsgContent[3],
+											 txMsgContent[4])
 
-					robotService = ArduinoService()
-					[x_idx, y_idx, x_enc, y_enc] = robotService.getDepositLocation()
+				elif msgType == MessageFormat.homeRobot:
+					msg = HomeRobotA2P()
+					msg.decode(msgContent)
 
-					msgInProgress = DepoInvP2A()
-					msgInProgress.barcode = barcode
-					msgInProgress.x_enc = x_enc
-					msgInProgress.y_enc = y_enc
+				elif msgType == MessageFormat.movRobot:
+					msg = MovPosA2P()
+					msg.decode(msgContent)
 
-					self.msgInProcess = [MessageFormat.depInv, (barcode, x_idx, y_idx, x_enc, y_enc)]
-
-				try:
-					encodedMsg = msgInProgress.encode()
-				except CorruptData as details:
-					print(details)
-
-				# send encodedMsg
-
-
-			## receive the message with blocking i/o
-			if self.isWaitingMsg:
-
-				msgParser = MessageParser()
-				isCompleteMsg = False
-
-				while not isCompleteMsg:
-					msgByte = self.__ser.read(1)		# blocking i/o with infinite timeout
-					parseResult = msgParser.parseMsg(msgByte)
-					isCompleteMsg = parseResult[0]
-
-				parseResult = msgParser.getRetVal()		# [True, msgType, msgContent]
-
-				msgType = parseResult[1]
-				msgContent = parseResult[2]
-
-				try:
-					robotService = ArduinoService()
-
-					if msgType == MessageFormat.retInv:
-						msg = RetInvA2P()
-						msg.decode(msgContent)
-						service.processRetrievedInventory(msg.invInfoRowId, msg.virtCartId)
-
-					elif msgType == MessageFormat.depInv:
-						msg = DepInvA2P()
-						msg.decode(msgContent)
-						#(barcode, x_index, y_index, x_encoder, y_encoder):
-						msgInProgressContent = msgInProgress[1]
-						service.depositInventory(msgInProgressContent[0],
-												 msgInProgressContent[1],
-												 msgInProgressContent[2],
-												 msgInProgressContent[3],
-												 msgInProgressContent[4])
-
-					elif msgType == MessageFormat.homeRobot:
-						msg = HomeRobotA2P()
-						msg.decode(msgContent)
-
-					elif msgType == MessageFormat.movRobot:
-						msg = MovPosA2P()
-						msg.decode(msgContent)
-
-				except CorruptData as details:
-					print(details)
+			except CorruptData as details:
+				print(details)
